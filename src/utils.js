775aa982dbc4e41e303d61986c380a058749ea68
Kelly Dwan <kelly.dwan@gmail.com>
2016-12-31 13:44:22 -0500

    Remove unnecessary query params when requesting from wp-api (leftover from forked wp.com code)

--- src/utils.js
+++ src/utils.js
@@ -5,13 +5,11 @@ import omitBy from 'lodash/omitBy';
 
 const DEFAULT_POST_QUERY = {
 	_embed: true,
-	number: 10,
 	offset: 0,
 	order: 'DESC',
 	order_by: 'date',
 	type: 'post',
 	status: 'publish',
-	sticky: 'include',
 	search: ''
 };
 
